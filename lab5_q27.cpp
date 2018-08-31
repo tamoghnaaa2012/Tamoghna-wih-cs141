715e918f8947a67183332e0855fe6e584b7e1674
Tamoghna Pattanayak <42137018+tamoghnaaa2012@users.noreply.github.com>
2018-08-31 07:18:12 +0000

    Add files via upload

---
+++ lab5_q27.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n,i=1,sum= 0;
+  cout<<"enter the value of n";
+  cin>>n;
+  cout<<"<< n <<""natural numbers are";
+
+  while(i<=n)
+
+  {
+   cout <<"<< i>>";
+ sum=sum+i;
+ i++;
+
+  
+   }
+cout<< "the sum i ="<< sum;
+return 0;
+}
