eb8c15d8cbf6d35f6757cab3def319d5e0e56013
Tamoghna Pattanayak <42137018+tamoghnaaa2012@users.noreply.github.com>
2018-08-31 07:17:16 +0000

    Add files via upload

---
+++ lab5_q5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int number , remainder;
+
+	cout << "enter the number: ";
+	cin>> number;
+	remainder = number % 2;
+	if (remainder == 0)
+	 cout<< number << " is an even integer" << endl;
+	else
+	  cout << number << " is  an odd integer " << endl;
+ return 0;
+}
+
+
+
